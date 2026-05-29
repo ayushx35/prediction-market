c0c99aea9472ed6948ecf7af83463375bdf047a4
Ayushmaan Singh <108607737+ayushx35@users.noreply.github.com>
2026-05-30 02:24:14 +0530

    add endpoints

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,28 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
 
-app.get('/', (req, res) => {
-    res.send('<div> <b>hello</b> </div>');
+app.use(express.json);
+app.use(cors);
+// endpoints
+
+app.post('/buy', (req, res) => {
+
+});
+app.post('/sell', (req, res) => {
+
+});
+app.post('/split', (req, res) => {
+
+});
+app.post('/merge', (req, res) => {
+
+});
+app.get('/balance', (req, res) => {
+
+});
+app.get('/history', (req, res) => {
+
 });
 
 app.listen(3000, () => {
