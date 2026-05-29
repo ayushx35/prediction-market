860420c0ba795fe04dface0204b58bff5d0bb96e
Ayushmaan Singh <108607737+ayushx35@users.noreply.github.com>
2026-05-30 02:20:44 +0530

    start backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<div> <b>hello</b> </div>');
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+})
